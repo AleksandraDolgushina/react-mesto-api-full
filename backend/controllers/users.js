b2cee3aff10d747cb709babbf7cc20579e3d2cf6
Aleksandra Dolgushina <sashadolgushina@yandex.ru>
2021-07-16 22:49:44 +0300

    feat

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -121,7 +121,13 @@ module.exports.login = (req, res, next) => {
         httpOnly: true,
         sameSite: true,
         maxAge: 3600000 * 24 * 7,
-      }).send({ token });
+      }).send({
+        _id: user._id,
+        email: user.email,
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+      });
     })
     .catch(() => {
       next(new AuthentificationError('Неправильный адрес почты или пароль'));
